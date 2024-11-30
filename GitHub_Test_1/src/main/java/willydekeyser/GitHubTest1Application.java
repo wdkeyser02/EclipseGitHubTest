1cef6d5a6b5282c41157cc0adea02a809422b1ba
willydekeyser <ma074172@skynet.be>
2024-11-30 16:49:15 +0100

    Setup Test 1

---
+++ GitHub_Test_1/src/main/java/willydekeyser/GitHubTest1Application.java
@@ -0,0 +1,13 @@
+package willydekeyser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHubTest1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitHubTest1Application.class, args);
+	}
+
+}
